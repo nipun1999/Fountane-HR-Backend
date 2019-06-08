16ca2d755149a532bf10411b55d3a6b9ce70297b
CSALS <charansaicharan14@gmail.com>
2019-06-08 11:37:34 +0530

    Add controller functions

--- controllers/attendanceCtrl.js
+++ controllers/attendanceCtrl.js
@@ -4,7 +4,7 @@ var utilities = require("../utilities/utilities");
 
 
 
-async function create(req, res){
+async function createAttendance(req, res){
     
 
     try {
@@ -16,20 +16,30 @@ async function create(req, res){
             checkOut: req.body.checkOut,
             comments: req.body.comments
         };
+        //When employee checks in a new record will be created , checkOut will be empty but not null
+        if(create_obj.empCode!="" && create_obj.date!="" && create_obj.checkIn!="") {
+            let attendance_created = await db.public.attendanceobj.create(create_obj);
 
-        let attendance_created = await db.public.attendanceobj.create(create_obj);
-
-        res.status(200).json({
-            success: true,
-            attendanceobj: attendance_created
-        });
+            res.status(200).json({
+                success: true,
+                attendanceobj: attendance_created
+            });
+        }
+        else{
+            res.status(500).json({
+                success: false,
+                error: {
+                    message: "Please input value of all parameters"
+                }
+            });
+        }
 
     } catch(err) {
         console.log(err);
         res.status(500).json({
             success: false,
             error: {
-                message: "Internal Server Error",
+                message: "Please put all body parameters",
                 description: err.description
             }
         });
@@ -37,7 +47,121 @@ async function create(req, res){
 
 }
 
-async function get(req, res){
+async function updateCheckOut(req, res){
+    
+    let create_obj = {
+        empCode : req.body.empCode ,
+        checkOut : req.body.checkOut
+    }
+    try {
+        //
+        if(create_obj.empCode && create_obj.checkOut) {
+            let attendance_checkout = await db.public.attendanceobj.update({checkOut : create_obj.checkOut},{
+                where: {empCode: create_obj.empCode}
+            });
+            res.status(200).json({
+                success: true,
+                attendanceobj: attendance_checkout
+            }); //getting returned even if there is no emp code in the table
+        }
+        else{
+            res.status(500).json({
+                success: false,
+                error: {
+                    message: "Please input value of all parameters"
+                }
+            });
+        }
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: "Please put all body parameters",
+                description: err.description
+            }
+        });
+    }
+
+}
+
+async function addComment(req, res){
+    
+    let create_obj = {
+        empCode : req.body.empCode ,
+        comments : req.body.comments
+    }
+    try {
+        //
+        if(create_obj.empCode && create_obj.comments!="") {
+            let attendance_comment = await db.public.attendanceobj.update({comments : create_obj.comments},{
+                where: {empCode: create_obj.empCode}
+            });
+
+            res.status(200).json({
+                success: true,
+                attendanceobj: create_obj
+            });
+        }
+        else{
+            res.status(500).json({
+                success: false,
+                error: {
+                    message: "Please input value of all parameters"
+                }
+            });
+        }
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: "Please put all body parameters",
+                description: err.description
+            }
+        });
+    }
+
+}
+
+async function deleteComment(req, res){
+
+    try {
+        //
+        if(req.body.empCode) {
+            let attendance_delete = await db.public.attendanceobj.update({comments: ""},{
+                where: {empCode: req.body.empCode}
+            });
+
+            res.status(200).json({
+                success: true,
+                attendanceobj: attendance_delete
+            });
+        }
+        else{
+            res.status(500).json({
+                success: false,
+                error: {
+                    message: "Please input value of all parameters"
+                }
+            });
+        }
+
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: "Please put all body parameters",
+                description: err.description
+            }
+        });
+    }
+
+}
+async function getEmployeeAttendanceByCode(req, res){
 
     try {
         //
@@ -48,15 +172,14 @@ async function get(req, res){
         }
 
         
-        if(req.query.date){
-            query.date = req.query.date;
-        }
+        // if(req.query.date){
+        //     query.date = req.query.date;
+        // }
 
         let values = await db.public.attendanceobj.findAll({
             where: query
         })
-
-
+        // if(Object.keys(values).length==0) Empty object condition
         res.status(200).json({
             success: true,
             attendanceobj: values
@@ -74,17 +197,87 @@ async function get(req, res){
     }
 
 }
-//Add update,delete comment
-async function updateComment(req, res){
-    
+
+async function getEmployeeAttendanceByDate(req, res){
+
+    try {
+        //
+        let query = {};
+
+        // if(req.query.empCode){
+        //     query.empCode = req.query.empCode;
+        // }
+
+        
+        if(req.query.date){
+            query.date = req.query.date;
+        }
+
+        let values = await db.public.attendanceobj.findAll({
+            where: query
+        })
+        // if(Object.keys(values).length==0) Empty object condition
+        res.status(200).json({
+            success: true,
+            attendanceobj: values
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: "Internal Server Error",
+                description: err.description
+            }
+        });
+    }
+
 }
-async function deleteComment(req, res){
-    
+
+async function getByCodeAndDate(req, res){
+
+    try {
+        //
+        let query = {};
+
+        if(req.query.empCode){
+            query.empCode = req.query.empCode;
+        }
+
+        
+        if(req.query.date){
+            query.date = req.query.date;
+        }
+
+        let values = await db.public.attendanceobj.findAll({
+            where: query
+        })
+        // if(Object.keys(values).length==0) Empty object condition
+        res.status(200).json({
+            success: true,
+            attendanceobj: values
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            error: {
+                message: "Internal Server Error",
+                description: err.description
+            }
+        });
+    }
+
 }
 
 module.exports = {
-    create,
-    get,
-    updateComment,
-    deleteComment
+    createAttendance,
+    updateCheckOut,
+    addComment,
+    deleteComment,
+    getEmployeeAttendanceByCode,
+    getEmployeeAttendanceByDate,
+    getByCodeAndDate
 }
