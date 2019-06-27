908e79a5e5cb6e9e80dd873203a34c9e0e1ee98d
CSALS <charansaicharan14@gmail.com>
2019-06-27 13:29:06 +0530

    Returning empCode

--- controllers/signInCtrl.js
+++ controllers/signInCtrl.js
@@ -129,6 +129,7 @@ async function checkUserGoogle(req, res){
             
                     res.status(200).json({
                         success: true,
+                        empCode : user.empCode,
                         token: token,
                         status : newUserStatus
                     });
