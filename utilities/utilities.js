ec952fdb08d83ee088fc92caf75efaba41a1012a
Bhavya Gupta <bhavyagupta@live.com>
2019-07-04 20:55:56 +0530

    update

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -22,7 +22,7 @@ module.exports.decryptJWTWithToken = function (token) {
 }
 
 module.exports.verifyRole = async function(roleId,action,entityName) {
-    return true;
+    return false;
     if(!roleId || !entityName) {
         return false;
     }
