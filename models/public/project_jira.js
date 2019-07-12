60be2384dcfa3f3e21c94d4025a4dc8654f2af02
CSALS <charansaicharan14@gmail.com>
2019-07-12 12:13:59 +0530

    new schema for projects

--- models/public/project_jira.js
+++ models/public/project_jira.js
@@ -1,30 +1,68 @@
-module.exports = (sequelize, DataTypes) => {
-    const project_jira = sequelize.define('project_jira', {
+// module.exports = (sequelize, DataTypes) => {
+//     const project_jira = sequelize.define('project_jira', {
         
 
-        projectID: {
-            type:DataTypes.BIGINT,
-            primaryKey : true,
-            allowNull : false
-        },
+//         projectKey: {
+//             type:DataTypes.STRING,
+//             primaryKey : true,
+//             allowNull : false
+//         },
 
-        projectName: {
-            type:DataTypes.STRING,
-            allowNull : false
-        },
+//         projectName: {
+//             type:DataTypes.STRING,
+//             allowNull : false
+//         },
         
 
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: new Date()
-        },
-    }, {
-            underscored: true
-        });
+//         created_at: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//             defaultValue: new Date()
+//         },
+//     }, {
+//             underscored: true
+//         });
 
-    project_jira.addHook('afterCreate', 'postOnboarding', (permission, options) => {
-        console.log("Data to be written in redis here");
+//     project_jira.addHook('afterCreate', 'postOnboarding', (permission, options) => {
+//         console.log("Data to be written in redis here");
+//     });
+//     return project_jira;
+// };
+
+
+
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('fountanehrdb', 'postgres', 'fountane', {
+    host: 'localhost',
+    dialect: 'postgres'
+  });
+const DataTypes = Sequelize
+  const project_jira = sequelize.define('project_jira', {
+        
+
+    projectKey: {
+        type:DataTypes.STRING,
+        primaryKey : true,
+        allowNull : false
+    },
+
+    projectName: {
+        type:DataTypes.STRING,
+        allowNull : false
+    },
+    
+
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date()
+    },
+}, {
+        underscored: true
     });
-    return project_jira;
-};
+    project_jira.addHook('afterCreate', 'postOnboarding', (permission, options) => {
+    console.log("Data to be written in redis here");
+});
+
+project_jira.sync({force:true})
