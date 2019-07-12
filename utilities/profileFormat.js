d0fe0b0516adcc448ca61d4c6991138986d2e26b
CSALS <charansaicharan14@gmail.com>
2019-07-12 16:19:59 +0530

    profile format updated

--- utilities/profileFormat.js
+++ utilities/profileFormat.js
@@ -6,19 +6,27 @@ async function get() {
     let result = await db.public.sequelize.query(query,{
         type: db.public.sequelize.QueryTypes.SELECT
     })
+    let m = 0;
+    let d = 0;
     result = result.reduce((acc, obj)=>{
         let department = "Delvelopment";
+        let i = 0;
         if(obj.department == "backend" || obj.department == "frontend"){
             department = "Delvelopment";
+            d += 1;
+            i=d
         }else{
             department = "Marketing";
+            m += 1;
+            i=m;
         }
         acc[department] = acc[department] ? acc[department] : [];
         acc[department].push({
             "Employee Name":obj.name,
             "Employee ID":obj.empCode,
             "Employee Location":obj.branchLocation,
-            "Employee Designation":obj.designation
+            "Employee Designation":obj.designation,
+            "Employee Code": `D${i}`
         });
         return acc;
     }, {})
