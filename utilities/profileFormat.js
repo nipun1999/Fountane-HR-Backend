020b630e612c64c172d1effe96966101fcfdbe9b
CSALS <charansaicharan14@gmail.com>
2019-07-12 16:49:11 +0530

    profile format updated

--- utilities/profileFormat.js
+++ utilities/profileFormat.js
@@ -8,34 +8,39 @@ async function get() {
     })
     let m = 0;
     let d = 0;
-    result = result.reduce((acc, obj)=>{
-        let department = "Delvelopment";
-        let i = 0;
-        if(obj.department == "backend" || obj.department == "frontend"){
-            department = "Delvelopment";
-            d += 1;
-            i=d
-        }else{
-            department = "Marketing";
-            m += 1;
-            i=m;
-        }
-        acc[department] = acc[department] ? acc[department] : [];
-        let newQuery = `select distinct(project_jiras."projectName") as projectName from project_jiras inner join projects on project_jiras."projectKey" = projects."projectKey" where projects."assigneeEmail"='${obj.fountaneEmail}'`
-        let newResult = await db.public.sequelize.query(newQuery,{
-            type: db.public.sequelize.QueryTypes.SELECT
-        })
-        acc[department].push({
-            "Employee Name":obj.name,
-            "Employee ID":obj.empCode,
-            "Employee Location":obj.branchLocation,
-            "Employee Designation":obj.designation,
-            "Employee Code": `D${i}`,
-            "Employee Current Projects" : newResult
+    let ress = await (async (result)=>{
+        return new Promise(async (resolve, reject)=>{
+            result = result.reduce((acc, obj)=>{
+                let department = "Delvelopment";
+                let i = 0;
+                if(obj.department == "backend" || obj.department == "frontend"){
+                    department = "Delvelopment";
+                    d += 1;
+                    i=d
+                }else{
+                    department = "Marketing";
+                    m += 1;
+                    i=m;
+                }
+                acc[department] = acc[department] ? acc[department] : [];
+                let newQuery = `select distinct(project_jiras."projectName") as projectName from project_jiras inner join projects on project_jiras."projectKey" = projects."projectKey" where projects."assigneeEmail"='${obj.fountaneEmail}'`
+                let newResult = await db.public.sequelize.query(newQuery,{
+                    type: db.public.sequelize.QueryTypes.SELECT
+                })
+                acc[department].push({
+                    "Employee Name":obj.name,
+                    "Employee ID":obj.empCode,
+                    "Employee Location":obj.branchLocation,
+                    "Employee Designation":obj.designation,
+                    "Employee Code": `D${i}`,
+                    "Employee Current Projects" : newResult
+                });
+                return acc;
+            }, {});
+            resolve(result);
         });
-        return acc;
-    }, {})
-    console.log(result)
+    })(result)
+    console.log(ress);
 }
 
 get()
