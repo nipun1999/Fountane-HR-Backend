6498a8053cc4d5c585b382b4289c5a3549de7935
ashwinkjoseph <ashwinkjoseph>
2019-07-29 19:31:53 +0530

    changed the relationship to project_jira instead of project

--- models/db.js
+++ models/db.js
@@ -44,7 +44,7 @@ db.public.team.belongsTo(db.public.register,{foreignKey:'empCode', onDelete:"CAS
 db.public.team.belongsTo(db.public.register,{foreignKey:'TLempCode', onDelete:"CASCADE"});
 db.public.signInObj.belongsTo(db.public.register,{foreignKey:'empCode', onDelete:"CASCADE"});
 
-db.public.project_updates.belongsTo(db.public.project, {foreignKey: 'project_id', onDelete: 'CASCADE'});
+db.public.project_updates.belongsTo(db.public.project_jira, {foreignKey: 'project_id', onDelete: 'CASCADE'});
 
 
 // Hooks come here
